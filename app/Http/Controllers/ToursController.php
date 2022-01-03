57518bb5551c5402dc8cca5acf98530693f9a349
Azman Abdullah <azmanabdlh@yahoo.com>
2022-01-03 23:08:53 +0900

    Menambahkan halaman wisata

--- app/Http/Controllers/ToursController.php
+++ app/Http/Controllers/ToursController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tour;
 use Illuminate\Http\Request;
 
 class ToursController extends Controller
 {
-    //
+    public function index()
+    {
+        $tours = Tour::latest()->paginate(9);
+
+        return view('potencies.tours.index', compact('tours'));
+    }
+
+    public function show(string $slug)
+    {
+        // $tour = Tour::whereSlug($slug)->first();
+        // abort_unless(
+        //     !is_null($tour),
+        //     404,
+        //     "Wista tidak ditemukan!"
+        // );
+
+        return view('potencies.tours.show');
+    }
 }
---
+++ resources/views/potencies/tours/index.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('head')
+<style>
+    .page-title .page-title-heading, .page-title .breadcrumbs {
+        float: none !important;
+    }
+    .page-title .breadcrumbs {
+        margin-top: .2rem;
+    }
+
+    .content-post .title-post {
+        line-height: 1.6rem;
+    }
+
+    .post .post-date .month {
+        font-size: .9rem !important;
+        padding: .2rem .5rem;
+    }
+
+    .post .content-post {
+        height: 16.25rem;
+        max-height: 16.25rem;
+    }
+
+    .post {
+        margin-bottom: 1rem !important;
+    }
+    .post:last-child {
+        margin-bottom: 0;
+    }
+    .not-found-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .not-found-container img {
+        width: 400px;
+    }
+    .not-found-container p {
+        font-size: 1.5rem;
+    }
+</style>
+@endsection
+@section('content')
+<div class="page-title">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="page-title-heading">
+                    <h1 class="h1-title">Informasi Wisata Negeri Hila</h1>
+                </div><!-- /.page-title-heading -->
+                <ul class="breadcrumbs">
+                    <li><a href="#" title="Beranda">Beranda<i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                    <li><a href="#" title="Beria">Wisata</a></li>
+                </ul><!-- /.breadcrumbs -->
+                <div class="clearfix"></div><!-- /.clearfix -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="main-content blog-single-post">
+    <div class="container">
+        <div class="row">
+            <div class="post-wrap">
+                @forelse ( $tours as $tour )
+                    <div class="col-md-4">
+                        <article class="post">
+                            <div class="featured-post">
+                                <a href="{{ route('news.show', $tour->slug) }}" title="{{ $tour->title }}" class="post-image">
+                                    <img src="{{ asset('storage/hello.jpg') }}" alt="img">
+                                </a>
+                                <ul class="post-date">
+                                    <li class="month">
+                                        {{ $tour->created_at->locale('id_ID')->isoFormat('LL') }}
+                                    </li>
+                                </ul>
+                            </div><!-- /.featured-post -->
+                            <div class="content-post">
+                                <h4 class="title-post">
+                                    <a href="#" title="">
+                                        {{ $tour->title }}
+                                    </a>
+                                </h4>
+                                <div class="entry-post">
+                                    <p>
+                                        {{ str($tour->content)->limit(80) }}
+                                    </p>
+                                    <div class="more-link">
+                                        <a href="{{ route('news.show', $tour->slug) }}" title="">Baca selengkapnya</a>
+                                    </div>
+                                </div><!-- /.entry-post -->
+                            </div><!-- /.content-post -->
+                        </article><!-- /.post -->
+                    </div>
+                @empty
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="not-found-container">
+                                    <img src="{{ asset('img/not-found-1.webp') }}" alt="not found"/>
+                                    <p>Wisata belum tersedia</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                @endforelse
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="dividers dividers-pagination"></div>
+                <div class="blog-single-pagination">
+                    {{ $tours->links('pagination::bootstrap-4-finance') }}
+                </div><!-- /.blog-pagination -->
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
